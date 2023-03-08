93f9b8385a9fb3829b8739106c4c028e3c1532a1
HUNSIKLEE <hunsik0206@gmail.com>
2023-03-08 18:08:49 +0900

    add: MVVM Todo

---
+++ Narin3/app/src/main/java/com/example/narin/viewmodel/TodoViewModel.kt
@@ -0,0 +1,29 @@
+package com.example.narin.viewmodel
+
+import android.app.Application
+import androidx.lifecycle.AndroidViewModel
+import androidx.lifecycle.LiveData
+import com.example.narin.data.TodoRepository
+import com.example.narin.model.TodoModel
+
+
+class TodoViewModel(application: Application) : AndroidViewModel(application) {
+
+    private val todoRepository = TodoRepository(application)
+    private var todoItems =
+        todoRepository.getTodoListAll()  //액티비티(View) 에서 ViewModel 의 todoItems 리스트를 observe 하고 리스트를 갱신
+
+    /* repsitory 에 넘겨 viewModel 의 기능 함수 구현 */
+
+    fun getTodoListAll(): LiveData<List<TodoModel>> {
+        return todoItems
+    }
+
+    fun insert(todoModel: TodoModel) {
+        todoRepository.insert(todoModel)
+    }
+
+    fun delete(todoModel: TodoModel) {
+        todoRepository.delete(todoModel)
+    }
+}

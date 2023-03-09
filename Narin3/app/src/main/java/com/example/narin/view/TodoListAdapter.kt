1950140ef1031cfc8e2ce7a65eb9d91cdaf633fb
HUNSIKLEE <hunsik0206@gmail.com>
2023-03-09 18:12:04 +0900

    feat: kotlin CRUD

--- Narin3/app/src/main/java/com/example/narin/view/TodoListAdapter.kt
+++ Narin3/app/src/main/java/com/example/narin/view/TodoListAdapter.kt
@@ -32,7 +32,8 @@ class TodoListAdapter(val deletetItemClick: (TodoModel) -> Unit) :
     * item_list 레이아웃을 사용하여 뷰를 생성하고 뷰홀더에 뷰를 전달하여 생성된 뷰홀더를 반환
     * */
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): TodoViewHolder {
-        val view = LayoutInflater.from(parent.context).inflate(R.layout.activity_main, parent, false)
+        val view =
+            LayoutInflater.from(parent.context).inflate(R.layout.activity_main, parent, false)
         val viewHolder = TodoViewHolder(view)
         return viewHolder
     }
@@ -69,15 +70,18 @@ class TodoListAdapter(val deletetItemClick: (TodoModel) -> Unit) :
 
         fun bind(todoModel: TodoModel) {
 
-            tv_seq.text = todoModel.seq.toString()
-            tv_title.text = todoModel.title
-            tv_content.text = todoModel.content
-            tv_date.text = todoModel.createDate.convertDateToString("yyyy.MM.dd HH:mm")
+            try {
+                tv_seq.text = todoModel.seq.toString()
+                tv_title.text = todoModel.title
+                tv_content.text = todoModel.content
+                tv_date.text = todoModel.createDate.convertDateToString("yyyy.MM.dd HH:mm")
 
-            iv_delete.setOnClickListener {
-                deletetItemClick(todoModel)
+                iv_delete.setOnClickListener {
+                    deletetItemClick(todoModel)
+                }
+            } catch (E: java.lang.Exception) {
+                Log.e("bind", "Error binding todo model : $todoModel", E)
             }
-
         }
 
     }
